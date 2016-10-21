0fe5790cf1b78908f9d3349cd77af71955db3512
Cesar David Castro Iniguez <Cesar David Castro Iniguez>
2016-10-20 19:21:07 -0500

    Factorial homework!

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int i;
+            int factor = 1;
+
+            Console.Write("Enter your number: ");
+            x = Int32.Parse(Console.ReadLine());
+
+            for (i = 1; i <= x; i++)
+            {
+                factor = factor * i;
+            }
+
+            Console.Write("Factorial is: ");
+            Console.WriteLine(factor);
+            Console.ReadLine();
+        }
+    }
+}
