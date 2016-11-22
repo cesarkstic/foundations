6db7649c05c9618fa1ca5e3e5da75b314a2fd8d8
Cesar David Castro Iniguez <Cesar David Castro Iniguez>
2016-11-22 17:40:57 -0600

    Assignment #4: Looping and Arrays

---
+++ Assignment4/Assignment4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int i;
+            int factor = 1;
+
+            Console.Write("Enter your number: ");
+            x = Int32.Parse(Console.ReadLine());
+
+            for (i = 1; i <= x; i++)
+            {
+                factor = factor * i;
+                Console.WriteLine ("{0}!  = {1}", i.ToString(), factor.ToString());
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
